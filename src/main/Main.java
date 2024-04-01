ba2a0e773f66845cc33ceb4efa8310650ff0e5bd
Alessandro Vaprio <alessandro.vaprio@elvispos.com>
2024-04-01 21:30:28 +0200

    more similar to UPO example. It needs refactor.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,19 +1,15 @@
 package main;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
+import awt.PannelloCtrl;
 
-
-import mqtt.MqttManager;
 
 public class Main {
 
 	public static void main (String[] args) {
 		try {
-			// mi collego in locale alla porta 1883 chiamandomi myjavaApp e collegandomi alla coda test_sensor_data
-			MqttManager mqtt = MqttManager.create("tcp://127.0.0.1:1883", "myjavaAPP", "test_sensor_data");
-			// pubblico il messaggio "HELLO THERE" dentro alla coda test_sensor_data
-			mqtt.postOnQueue("test_sensor_data", "HELLO THERE");
-		} catch (MqttException e) {
+			PannelloCtrl pannello = new PannelloCtrl();
+			pannello.start();
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
